ec6b2e8bc08a594d4e1fdc61fa14847fe3fd1c6f
Daniel Tuy <danielitotuchez100@gmail.com>
2024-05-18 01:54:53 -0600

    Crud TelefonoProveedor completo con boton eliminar junto con el crud producto, siempre seleccionar foreign key para eliminar los datos, cuando queramos editar tenemos que editar todos los datos para que se guarden en la base de datos

--- src/org/danieltuy/controller/MenuTelefonoProveedorController.java
+++ src/org/danieltuy/controller/MenuTelefonoProveedorController.java
@@ -1,11 +1,27 @@
 package org.danieltuy.controller;
 
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javax.swing.JOptionPane;
+import org.danieltuy.bean.Proveedores;
+import org.danieltuy.bean.TelefonoProveedor;
+import org.danieltuy.db.Conexion;
 import org.danieltuy.system.Main;
 
 /**
@@ -15,12 +31,325 @@ import org.danieltuy.system.Main;
 public class MenuTelefonoProveedorController implements Initializable {
 
     private Main escenarioPrincipal;
+
+    private enum operaciones {
+        AGREGAR, ELIMINAR, EDITAR, ACTUALIZAR, CANCELAR, NINGUNO
+    }
+    private operaciones tipoDeOperaciones = operaciones.NINGUNO;
+    private ObservableList<TelefonoProveedor> listaTelefonoProveedor;
+    private ObservableList<Proveedores> listaProveedores;
+
     @FXML
     private Button btnRegresar;
+    @FXML
+    private TextField txtCodigoTelProve;
+    @FXML
+    private TextField txtNumPrinProve;
+    @FXML
+    private TextField txtNumSecunProve;
+    @FXML
+    private TextField txtObsProve;
+    @FXML
+    private ComboBox cmbCodProvProve;
+    @FXML
+    private TableView tblTelefonoProveedor;
+    @FXML
+    private TableColumn colCodigoTelProve;
+    @FXML
+    private TableColumn colNumPrinProve;
+    @FXML
+    private TableColumn colNumSecunProve;
+    @FXML
+    private TableColumn colObsProve;
+    @FXML
+    private TableColumn colCodProvProve;
+    @FXML
+    private Button btnEditar;
+    @FXML
+    private Button btnAgregar;
+    @FXML
+    private Button btnEliminar;
+    @FXML
+    private Button btnReporte;
+    @FXML
+    private ImageView imgEditar;
+    @FXML
+    private ImageView imgAgregar;
+    @FXML
+    private ImageView imgEliminar;
+    @FXML
+    private ImageView imgReporte;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        cargarDatos();
+        cmbCodProvProve.setItems(getProveedores());
+    }
 
+    public void cargarDatos() {
+        tblTelefonoProveedor.setItems(getTelefonoProveedor());
+        colCodigoTelProve.setCellValueFactory(new PropertyValueFactory<TelefonoProveedor, Integer>("codigoTelefonoProveedor"));
+        colNumPrinProve.setCellValueFactory(new PropertyValueFactory<TelefonoProveedor, String>("numeroPrincipal"));
+        colNumSecunProve.setCellValueFactory(new PropertyValueFactory<TelefonoProveedor, String>("numeroSecundario"));
+        colObsProve.setCellValueFactory(new PropertyValueFactory<TelefonoProveedor, String>("observaciones"));
+        colCodProvProve.setCellValueFactory(new PropertyValueFactory<TelefonoProveedor, Integer>("codigoProveedor"));
+
+    }
+
+    public void seleccionarElementos() {
+        txtCodigoTelProve.setText(String.valueOf(((TelefonoProveedor) tblTelefonoProveedor.getSelectionModel().getSelectedItem()).getCodigoTelefonoProveedor()));
+        txtNumPrinProve.setText(((TelefonoProveedor) tblTelefonoProveedor.getSelectionModel().getSelectedItem()).getNumeroPrincipal());
+        txtNumSecunProve.setText(((TelefonoProveedor) tblTelefonoProveedor.getSelectionModel().getSelectedItem()).getNumeroSecundario());
+        txtObsProve.setText(((TelefonoProveedor) tblTelefonoProveedor.getSelectionModel().getSelectedItem()).getObservaciones());
+
+    }
+
+    public Proveedores buscarProveedores(int codigoProveedor) {
+        Proveedores resultado = null;
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("{call sp_buscarProveedores()}");
+            procedimiento.setInt(1, codigoProveedor);
+            ResultSet registro = procedimiento.executeQuery();
+            while (registro.next()) {
+                resultado = new Proveedores(registro.getInt("codigoProveedor"),
+                        registro.getString("NITProveedor"),
+                        registro.getString("nombresProveedor"),
+                        registro.getString("apellidosProveedor"),
+                        registro.getString("direccionProveedor"),
+                        registro.getString("razonSocial"),
+                        registro.getString("contactoPrincipal"),
+                        registro.getString("paginaWeb"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultado;
+    }
+
+    public ObservableList<TelefonoProveedor> getTelefonoProveedor() {
+        ArrayList<TelefonoProveedor> lista = new ArrayList<TelefonoProveedor>();
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("{call sp_listarTelefonoProveedor()}");
+            ResultSet resultado = procedimiento.executeQuery();
+            while (resultado.next()) {
+                lista.add(new TelefonoProveedor(resultado.getInt("codigoTelefonoProveedor"),
+                        resultado.getString("numeroPrincipal"),
+                        resultado.getString("numeroSecundario"),
+                        resultado.getString("observaciones"),
+                        resultado.getInt("codigoProveedor")));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listaTelefonoProveedor = FXCollections.observableArrayList(lista);
+
+    }
+
+    public ObservableList<Proveedores> getProveedores() {
+        ArrayList<Proveedores> lista = new ArrayList<>();
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("{call sp_listarProveedores()}");
+            ResultSet resultado = procedimiento.executeQuery();
+            while (resultado.next()) {
+                lista.add(new Proveedores(resultado.getInt("codigoProveedor"),
+                        resultado.getString("NITProveedor"),
+                        resultado.getString("nombresProveedor"),
+                        resultado.getString("apellidosProveedor"),
+                        resultado.getString("direccionProveedor"),
+                        resultado.getString("razonSocial"),
+                        resultado.getString("contactoPrincipal"),
+                        resultado.getString("paginaWeb")));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listaProveedores = FXCollections.observableList(lista);
+    }
+
+    public void agregar() {
+        switch (tipoDeOperaciones) {
+            case NINGUNO:
+                activarControles();
+                btnAgregar.setText("Guardar");
+                btnEliminar.setText("Eliminar");
+                btnEditar.setDisable(true);
+                btnReporte.setDisable(true);
+                imgAgregar.setImage(new Image("/org/danieltuy/images/ImagenGuardar.png"));
+                imgEliminar.setImage(new Image("/org/danieltuy/images/ImagenCancelar.png"));
+                tipoDeOperaciones = operaciones.ACTUALIZAR;
+                break;
+            case ACTUALIZAR:
+                guardar();
+                desactivarControles();
+                limpiarControles();
+                btnAgregar.setText("Guardar");
+                btnEliminar.setText("Eliminar");
+                btnEditar.setDisable(false);
+                btnReporte.setDisable(false);
+                imgAgregar.setImage(new Image("/org/danieltuy/images/Agregar.png"));
+                imgEliminar.setImage(new Image("/org/danieltuy/images/Eliminar.png"));
+                tipoDeOperaciones = operaciones.NINGUNO;
+                cargarDatos();
+                break;
+        }
+
+    }
+
+    // COLOCARLO EN ORDEN COMO ESTA LA BASE DE DATOS PARA QUE NO CRASHEE EL PROGRAMA
+    public void guardar() {
+        TelefonoProveedor registro = new TelefonoProveedor();
+        registro.setCodigoTelefonoProveedor(Integer.parseInt(txtCodigoTelProve.getText()));
+        registro.setNumeroPrincipal(txtNumPrinProve.getText());
+        registro.setNumeroSecundario(txtNumSecunProve.getText());
+        registro.setObservaciones(txtObsProve.getText());
+        registro.setCodigoProveedor(((Proveedores) cmbCodProvProve.getSelectionModel().getSelectedItem()).getCodigoProveedor());
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("{call sp_agregarTelefonoProveedor(?, ?, ?, ?, ?)}");
+            procedimiento.setInt(1, registro.getCodigoTelefonoProveedor());
+            procedimiento.setString(2, registro.getNumeroPrincipal());
+            procedimiento.setString(3, registro.getNumeroSecundario());
+            procedimiento.setString(4, registro.getObservaciones());
+            procedimiento.setInt(5, registro.getCodigoProveedor());
+            procedimiento.execute();
+
+            listaTelefonoProveedor.add(registro);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void eliminar() {
+        switch (tipoDeOperaciones) {
+            case ACTUALIZAR:
+                desactivarControles();
+                limpiarControles();
+                btnAgregar.setText("Agregar");
+                btnEliminar.setText("eliminar");
+                btnEditar.setDisable(false);
+                btnReporte.setDisable(false);
+                imgAgregar.setImage(new Image("/org/danieltuy/images/Agregar.png"));
+                imgEliminar.setImage(new Image("/org/danieltuy/images/Eliminar.png"));
+                tipoDeOperaciones = operaciones.NINGUNO;
+                break;
+            default:
+                if (tblTelefonoProveedor.getSelectionModel().getSelectedItem() != null) {
+                    int respuesta = JOptionPane.showConfirmDialog(null, "Confirmar la eliminacion del registro",
+                            "Eliminar telefono proveedor", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                    if (respuesta == JOptionPane.YES_NO_OPTION) {
+                        try {
+                            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("{call sp_eliminarTelefonoProveedor(?)}");
+                            procedimiento.setInt(1, ((TelefonoProveedor) tblTelefonoProveedor.getSelectionModel().getSelectedItem()).getCodigoTelefonoProveedor());
+                            procedimiento.execute();
+                            listaTelefonoProveedor.remove(tblTelefonoProveedor.getSelectionModel().getSelectedItem());
+                            limpiarControles();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+                } else {
+                    JOptionPane.showConfirmDialog(null, "Debe de seleccionar un producto para eliminar");
+                }
+        }
+    }
+
+    public void editar() {
+        switch (tipoDeOperaciones) {
+            case NINGUNO:
+                if (tblTelefonoProveedor.getSelectionModel().getSelectedItem() != null) {
+                    btnEditar.setText("Actualizar");
+                    btnReporte.setText("Cancelar");
+                    btnAgregar.setDisable(true);
+                    btnEliminar.setDisable(true);
+                    imgEditar.setImage(new Image("/org/danieltuy/images/Actualizar.png"));
+                    imgReporte.setImage(new Image("/org/danieltuy/images/ImagenCancelar.png"));
+                    activarControles();
+                    txtCodigoTelProve.setEditable(false);
+                    tipoDeOperaciones = operaciones.ACTUALIZAR;
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "Debe de seleccionar el proveedor para editar");
+                }
+                break;
+            case ACTUALIZAR:
+                actualizar();
+                btnEditar.setText("Editar");
+                btnReporte.setText("Reporte");
+                btnAgregar.setDisable(false);
+                btnEliminar.setDisable(false);
+                imgEditar.setImage(new Image("/org/danieltuy/images/Editar.png"));
+                imgReporte.setImage(new Image("/org/danieltuy/images/Reporte.png"));
+                desactivarControles();
+                limpiarControles();
+                tipoDeOperaciones = operaciones.NINGUNO;
+                cargarDatos();
+                break;
+        }
+    }
+
+    public void actualizar() {
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("{call sp_actualizarTelefonoProveedor(?, ?, ?, ?, ?)}");
+            TelefonoProveedor registro = (TelefonoProveedor) tblTelefonoProveedor.getSelectionModel().getSelectedItem();
+            registro.setCodigoTelefonoProveedor(Integer.parseInt(txtCodigoTelProve.getText()));
+            registro.setNumeroPrincipal(txtNumPrinProve.getText());
+            registro.setNumeroSecundario(txtNumSecunProve.getText());
+            registro.setObservaciones(txtObsProve.getText());
+            registro.setCodigoProveedor(((Proveedores) cmbCodProvProve.getSelectionModel().getSelectedItem()).getCodigoProveedor());
+            procedimiento.setInt(1, registro.getCodigoTelefonoProveedor());
+            procedimiento.setString(2, registro.getNumeroPrincipal());
+            procedimiento.setString(3, registro.getNumeroSecundario());
+            procedimiento.setString(4, registro.getObservaciones());
+            procedimiento.setInt(5, registro.getCodigoProveedor());
+            procedimiento.execute();
+        } catch (Exception e) {
+            e.getMessage();
+            e.printStackTrace();
+        }
+    }
+
+    public void reportes() {
+        switch (tipoDeOperaciones) {
+            case ACTUALIZAR:
+                desactivarControles();
+                limpiarControles();
+                btnEditar.setText("Editar");
+                btnReporte.setText("Reporte");
+                btnAgregar.setDisable(false);
+                btnEliminar.setDisable(false);
+                imgEditar.setImage(new Image("/org/danieltuy/images/Editar.png"));
+                imgReporte.setImage(new Image("/org/danieltuy/images/Reporte.png"));
+                tipoDeOperaciones = operaciones.NINGUNO;
+                break;
+        }
+    }
+
+    public void desactivarControles() {
+        txtCodigoTelProve.setEditable(false);
+        txtNumPrinProve.setEditable(false);
+        txtNumSecunProve.setEditable(false);
+        txtObsProve.setEditable(false);
+        cmbCodProvProve.setDisable(true);
+
+    }
+
+    public void activarControles() {
+        txtCodigoTelProve.setEditable(true);
+        txtNumPrinProve.setEditable(true);
+        txtNumSecunProve.setEditable(true);
+        txtObsProve.setEditable(true);
+        cmbCodProvProve.setDisable(false);
+    }
+
+    public void limpiarControles() {
+        txtCodigoTelProve.clear();
+        txtNumPrinProve.clear();
+        txtNumSecunProve.clear();
+        txtObsProve.clear();
+        tblTelefonoProveedor.getSelectionModel().getSelectedItem();
+        cmbCodProvProve.getSelectionModel().getSelectedItem();
     }
 
     public void setEscenarioPrincipal(Main escenarioPrincipal) {
